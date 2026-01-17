03cfa866bb475f113e06908c510c980765b8da76
Thor B. <thorbm@outlook.dk>
2026-01-17 18:49:20 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+![inventar-logo](https://cdn.prodzeus.dev/brand-assets/inventar/logo/full-dark-mode.svg)
+
+#### An <strong>Open Source</strong> personal inventory management system.
---
+++ src/routes/+page.svelte
@@ -0,0 +1,45 @@
+<script>
+    import LogoFull from '$lib/assets/logos/Logo - Icon and Text.svg'
+</script>
+
+<section>
+    <img src="{LogoFull}" alt="inventar logo">
+    <p>Visit <a href="https://github.com/thorbm1500/inventar">github.com/thorbm1500/inventar</a> to read more</p>
+</section>
+
+<style>
+    section {
+        height: 100vh;
+        align-content: center;
+        justify-content: center;
+
+        img {
+            width: 28rem;
+            justify-self: center;
+        }
+
+        p {
+            margin-top: .5em;
+            color: #0d0d0d;
+            font-family: 'FunnelSans','ArchivoRegular', sans-serif;
+            text-align: center;
+            font-size: .9em;
+            font-variation-settings: "wght" 400;
+        }
+
+        p a {
+            font-variation-settings: "wght" 400;
+            transition-duration: 0.4s;
+        }
+
+        p a:hover {
+            font-variation-settings: "wght" 600;
+            transition-duration: 0.4s;
+        }
+
+        p a:active {
+            font-size: 1.25em;
+            transition-duration: 0.4s;
+        }
+    }
+</style>
