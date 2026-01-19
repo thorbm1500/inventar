9858ead590ca27d249f4f9648fcdfe70cfe27475
Thor B. <thorbm@outlook.dk>
2026-01-19 18:53:17 +0100

    Changed from Drizzle to postgres.js

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,57 +1,55 @@
-import {pgTable, text, uuid, varchar, bigint, smallint} from 'drizzle-orm/pg-core';
+export const INVENTORIES		 = `CREATE TABLE IF NOT EXISTS inventories 
+									(
+    								inventory_uuid UUID PRIMARY KEY DEFAULT uuidv7(),
+    								name VARCHAR(255) NOT NULL,
+    								description TEXT DEFAULT NULL
+									)`
 
-export const pgtInventories = pgTable('inventories', {
-	uuid: uuid('inventory_uuid').defaultRandom().primaryKey(),
-	name: varchar().notNull(),
-	description: text().notNull()
-});
+export const CATEGORIES      	 = `CREATE TABLE IF NOT EXISTS categories 
+									(
+    								category_uuid UUID PRIMARY KEY DEFAULT uuidv7(),
+									inventory_uuid UUID PRIMARY KEY,
+									name VARCHAR(255) NOT NULL,
+									description TEXT DEFAULT NULL,
+									FOREIGN KEY(inventory_uuid) REFERENCES inventories(inventory_uuid) ON DELETE CASCADE
+							   		)`
 
-export type Inventories = typeof pgtInventories.$inferSelect;
+export const CURRENCIES			 = `CREATE TABLE IF NOT EXISTS currencies
+                           			(
+                               		currency_code VARCHAR(3) PRIMARY KEY,
+								   	currency_number SMALLINT NOT NULL,
+								   	currency_symbol VARCHAR(255) DEFAULT NULL
+                           			)`
 
-export const pgtCategories = pgTable('categories', {
-	uuid: uuid('category_uuid').defaultRandom().primaryKey(),
-	inventory_uuid: uuid('inventory_uuid').primaryKey().references(() => pgtInventories.uuid),
-	name: varchar().notNull(),
-	description: text()
-});
+export const ITEMS 				 = `CREATE TABLE IF NOT EXISTS items 
+									(
+   		 							item_uuid UUID PRIMARY KEY DEFAULT uuidv7(),
+									name VARCHAR(255) NOT NULL,
+									description TEXT DEFAULT NULL,
+									url VARCHAR(255) DEFAULT NULL,
+									assets_path VARCHAR(255) DEFAULT NULL,
+									price BIGINT NOT NULL,
+									currency VARCHAR(3) DEFAULT 'EUR',
+									amount BIGINT DEFAULT 0,
+             		       	        FOREIGN KEY(currency) REFERENCES currencies(currency_code) ON DELETE CASCADE
+									)`
 
-export type Categories = typeof pgtCategories.$inferSelect;
+export const ITEM_CATEGORIES	 = `CREATE TABLE IF NOT EXISTS item_categories 
+									(
+									inventory_uuid UUID PRIMARY KEY,
+									item_uuid UUID PRIMARY KEY,
+									category_uuid UUID PRIMARY KEY,
+									FOREIGN KEY(inventory_uuid) REFERENCES inventories(inventory_uuid) ON DELETE CASCADE,
+									FOREIGN KEY(item_uuid) REFERENCES items(item_uuid) ON DELETE CASCADE,
+									FOREIGN KEY(category_uuid) REFERENCES categories(category_uuid) ON DELETE CASCADE
+									)`
 
-export const pgtItems = pgTable('items', {
-	uuid: uuid('item_uuid').defaultRandom().primaryKey(),
-	inventory_uuid: uuid('inventory_uuid').primaryKey().references(() => pgtInventories.uuid),
-	name: varchar().notNull(),
-	description: text(),
-	url: varchar(),
-	image_path: varchar('image'),
-	price: bigint({ mode: 'bigint' }).default(0n).notNull(),
-	currency: varchar('currency', { length: 3 }).references(() => pgtCurrencies.currency_code),
-	amount: bigint({ mode: 'bigint' }).default(0n).notNull(),
-});
-
-export type Items = typeof pgtItems.$inferSelect;
-
-export const pgtItemCategories = pgTable('item_categories', {
-	inventory_uuid: uuid().primaryKey().references(() => pgtInventories.uuid),
-	item_uuid: uuid().primaryKey().references(() => pgtItems.uuid),
-	category_uuid: uuid().primaryKey().references(() => pgtCategories.uuid),
-});
-
-export type ItemCategories = typeof pgtItemCategories.$inferSelect;
-
-export const pgtCurrencies = pgTable('currencies', {
-	currency_code: varchar({ length: 3 }).primaryKey(),
-	currency_number: smallint().notNull(),
-	currency_symbol: varchar(),
-});
-
-export type Currencies = typeof pgtCurrencies.$inferSelect;
-
-export const pgtItemsPendingChanges = pgTable('items_pending_changes', {
-	inventory_uuid: uuid().primaryKey().references(() => pgtInventories.uuid),
-	item_uuid: uuid().primaryKey().references(() => pgtItems.uuid),
-	in_order: bigint({ mode: 'bigint' }).default(0n).notNull(),
-	reserved: bigint({ mode: 'bigint' }).default(0n).notNull(),
-});
-
-export type ItemsPendingChanges = typeof pgtItemsPendingChanges.$inferSelect;
+export const PENDING_ITEM_CHANGES = `CREATE TABLE IF NOT EXISTS pending_item_changes
+									 (
+									 inventory_uuid UUID PRIMARY KEY,
+									 item_uuid UUID PRIMARY KEY,
+									 in_order BIGINT DEFAULT 0,
+									 reserved BIGINT DEFAULT 0,
+									 FOREIGN KEY(inventory_uuid) REFERENCES inventories(inventory_uuid) ON DELETE CASCADE,
+									 FOREIGN KEY(item_uuid) REFERENCES items(item_uuid) ON DELETE CASCADE	 
+									 )`
