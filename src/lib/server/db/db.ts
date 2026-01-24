c70aca65dd354366ec8e1e913aafac73da21da47
Thorbm1500 <thorbm@proton.me>
2026-01-24 14:13:12 +0100

    Ensured type safety on Number#parseInt, and refactored db.js to db.ts

--- src/lib/server/db/db.ts
+++ src/lib/server/db/db.ts
@@ -3,7 +3,7 @@ import {env} from "$env/dynamic/private";
 
 const sql = postgres({
     host: env.DB_HOST,
-    port: Number.parseInt(env.DB_PORT),
+    port: Number.parseInt(env.DB_PORT ?? 'NONE'),
     database: env.DB_DATABASE,
     username: env.DB_USER,
     password: env.DB_PASSWORD
