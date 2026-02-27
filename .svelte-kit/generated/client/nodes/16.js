fecd63d0186a3bbf5b8fe8e5da8b4c6fe9eabd77
prodzeus <43374833+thorbm1500@users.noreply.github.com>
2026-02-27 16:29:07 +0100

    Delete .svelte-kit directory

--- .svelte-kit/generated/client/nodes/16.js
+++
@@ -1,1 +0,0 @@
-export { default as component } from "../../../../src/routes/(app)/settings/[[category]]/[[subcategory]]/+page.svelte";
--- .svelte-kit/generated/client/nodes/7.js
+++
@@ -1,1 +0,0 @@
-export { default as component } from "../../../../src/routes/(app)/account/[id]/+page.svelte";
